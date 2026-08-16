a5747e83a26bbdfba0d23650295d1f4f0f4f3385
yiik223 <b.figueroa.baile@gmail.com>
2026-08-15 21:09:29 -0400

    Reestructuracion html e indexacion de rutas

---
+++ frontend/chimimanitas/app/contacto/page.tsx
@@ -0,0 +1,10 @@
+export default function Contacto() {
+  return (
+    <main>
+      <h1>Contáctanos</h1>
+      <p>
+        Si tienes alguna pregunta sobre nuestros productos, escríbenos.
+      </p>
+    </main>
+  );
+}
---
+++ frontend/chimimanitas/app/donaciones/page.tsx
@@ -0,0 +1,10 @@
+export default function Donaciones() {
+  return (
+    <main>
+      <h1>Apoya nuestro proyecto</h1>
+      <p>
+        Si te gusta nuestro trabajo, puedes apoyarnos con una donación.
+     </p>
+    </main>
+  );
+}
